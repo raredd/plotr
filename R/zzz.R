7a925243b340174e8ac7fe0aa7dbbea081ac1125
raredd <raredd01@gmail.com>
2015-04-13 12:53:17 -0400

    add utils for example code

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,22 @@
 ## utils
 
 '%||%' <- function(a, b) if (!is.null(a)) a else b
+
+'%ni%' <- function(x, table) !(match(x, table, nomatch = 0) > 0)
+
+## rawr::tcol
+#' @export
+tcol <- function(color, trans = 255) {
+  if (length(color) != length(trans) & 
+        !any(c(length(color), length(trans)) == 1)) 
+    stop('Vector lengths not correct')
+  if (length(color) == 1 & length(trans) > 1) 
+    color <- rep(color, length(trans))
+  if (length(trans) == 1 & length(color) > 1) 
+    trans <- rep(trans, length(color))
+  res <- paste0('#', apply(apply(rbind(col2rgb(color)), 2, function(x) 
+    format(as.hexmode(x), 2)), 2, paste, collapse = ''))
+  res <- unlist(unname(Map(paste0, res, as.character(as.hexmode(trans)))))
+  res[is.na(color)] <- NA
+  return(res)
+}
